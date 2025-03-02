b93f5f27d08dff2fb30e8445c626d557ae7ce2d1
mohanapriyan2006 <mohanapriyanyt@gmail.com>
2025-03-02 11:05:50 +0530

    portfolio-link

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="footer">
         <h5><img src={icon} alt="icon" style={{ height: '15px', width: '15px' ,marginRight:'5px'}} />m<i>ews</i></h5>
-        <p>&copy; 2025 | Developed by <b>Mohanapriyan.M</b>
+        <p>&copy; 2025 | Developed by <a style={{color: "white"}} href="https://mohanapriyan.netlify.app/"><b>Mohanapriyan.M</b></a>
         </p>
     </div>
   )
